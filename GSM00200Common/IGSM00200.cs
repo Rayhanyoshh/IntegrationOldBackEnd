bf29258da6d830ddfc14d62b5ebc558bc601fe68
Rayhanyoshh <Rayhan.yoshara@gmail.com>
2023-04-04 13:46:11 +0700

    Add project files.

---
+++ GSM00200Common/IGSM00200.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using GSM00200Comoon;
+using R_CommonFrontBackAPI;
+
+namespace GSM00200Common
+{
+    public interface IGSM00200 : R_IServiceCRUDBase<GSM00200DTO>
+    {
+        IAsyncEnumerable<GSM00200DTOnon> GetTableHDList();
+    }
+}
---
+++ GSM00200Common/IGSM00210.cs
@@ -0,0 +1,11 @@
+﻿using GSM00200Comoon;
+using System.Collections.Generic;
+using R_CommonFrontBackAPI;
+
+namespace GSM00200Common
+{
+    public interface IGSM00210 : R_IServiceCRUDBase<GSM00210DTO>
+    {
+        IAsyncEnumerable<GSM00210DTO> GetTableList();
+    }
+}
